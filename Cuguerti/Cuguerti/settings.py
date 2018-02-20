f6fd617d6a2cd7f20a33346574b057f59ace9f60
enrique <enrique.fernandez@a3sec.com>
2018-02-20 12:04:53 +0100

    Initial commit for api

--- Cuguerti/Cuguerti/settings.py
+++ Cuguerti/Cuguerti/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'restframework',
+    'users',
+    'purchases',
+    'tshirts'
 ]
 
 MIDDLEWARE = [
--- Cuguerti/users/tests.py
+++ Cuguerti/users/tests.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+
 class TryTestsTestCase(TestCase):
 
     def test_it_works(self):
