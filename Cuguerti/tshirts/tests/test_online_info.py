165f4b3dce2d60b1eb7949efaa6eab2b2afb1ad9
enrique <enrique.fernandez@a3sec.com>
2018-02-21 11:48:29 +0100

    Add 2 tests

--- Cuguerti/tshirts/tests/test_online_info.py
+++ Cuguerti/tshirts/tests/test_online_info.py
@@ -33,3 +33,28 @@ class OnlineInfoTestCase(TestCase):
 
         # Asserting information is the asked
         self.assertEquals(info, 'fakey_shirt')
+
+    @mock.patch('requests.get', return_value=MockedResponse(400, 'the_shirt_is_a_lie'))
+    def test_static_get_online_info_bad_response_ko(self, shirt_mock):
+
+        msg_regexp = r'^Invalid response\. Status: [1-5]\d{2}$'
+        with self.assertRaisesRegexp(ValueError, msg_regexp):
+            # Asking for information
+            info = OnlineInformation.get_online_info('fake-site/the_shirt_is_a_lie')
+            # we can do more things here and it will assert in some point of
+            # the whole context it raises te desired exception
+
+    @mock.patch('requests.get',
+                return_value=MockedResponse(400, 'the_shirt_is_a_lie'))
+    def test_static_get_online_info_bad_response_ko(self, shirt_mock):
+
+        msg_regexp = r'^Invalid response\. Status: [1-5]\d{2}$'
+
+        # Asserts that when the callable_obj is called with the args raises the
+        # exception
+        self.assertRaisesRegexp(
+            ValueError,
+            msg_regexp,
+            OnlineInformation.get_online_info,  # callable_obj
+            *('fake-site/the_shirt_is_a_lie',)  # args for the call
+        )
